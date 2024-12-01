20e4272563dc611c88287be13418ac85e65a6c63
Sawaki Shuto <swkshuto0208@icloud.com>
2024-12-02 01:14:10 +0900

    CRUD追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,47 @@
 class PostsController < ApplicationController
-end
+    # 全投稿を取得
+    def index
+      posts = Post.all
+      render json: posts
+    end
+  
+    # 特定の投稿を取得
+    def show
+      post = Post.find(params[:id])
+      render json: post
+    end
+  
+    # 新しい投稿を作成
+    def create
+      post = Post.new(post_params)
+      if post.save
+        render json: post, status: :created
+      else
+        render json: post.errors, status: :unprocessable_entity
+      end
+    end
+  
+    # 投稿を更新
+    def update
+      post = Post.find(params[:id])
+      if post.update(post_params)
+        render json: post
+      else
+        render json: post.errors, status: :unprocessable_entity
+      end
+    end
+  
+    # 投稿を削除
+    def destroy
+      post = Post.find(params[:id])
+      post.destroy
+      head :no_content
+    end
+  
+    private
+  
+    def post_params
+      params.require(:post).permit(:title, :content)
+    end
+  end
+  
